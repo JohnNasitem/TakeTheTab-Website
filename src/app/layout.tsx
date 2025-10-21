43cd9c899578c1c70603351397a6db518a7e455e
JohnNasitem <jnasitem1@nait.ca>
2025-10-21 14:23:19 -0600

    added vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import { AppProviders } from "@/components/providers/app-providers";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <SpeedInsights/>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <div className="text-[var(--color-foreground)] bg-[var(--color-background)] flex flex-col min-h-screen">
           <Navbar links={navbarLinks} logo="/vercel.svg" />
