80fb4aa173cb7d41060dc2edb1d87169938b6b2f
JohnNasitem <jc.nasitem@gmail.com>
2025-10-29 13:34:35 -0600

    added ability to go to friend user view when friend item is clicked

--- src/components/friends-item.tsx
+++ src/components/friends-item.tsx
@@ -1,5 +1,6 @@
 import { FriendItemProps } from "@/interfaces/parameters"
 import { FriendsApiService } from "@/components/server-api-serivces/friends-api-service"
+import Link from 'next/link';
 
 const FriendItem = ({ userId, displayName, email, onRemove }: FriendItemProps) => {
     const { removeFriend } = FriendsApiService();
@@ -21,7 +22,8 @@ const FriendItem = ({ userId, displayName, email, onRemove }: FriendItemProps) =
     }
 
     return (
-        <div
+        <Link
+            href={`friends/${userId}`}
             className="grid grid-cols-[1fr_auto] font-[family-name:var(--font-geist-mono)] rounded-3xl bg-[var(--color-bg-accent)] hover:bg-[var(--color-background)] p-4 md:p-5 text-2xl md:text-3xl w-full h-max">
             <div className='pr-4 h-full flex items-center grid grid-rows-2 md:grid-rows-1 md:grid-cols-[auto_auto_auto_1fr] gap-3'>
                 <span className="truncate">{displayName}</span>
@@ -29,14 +31,16 @@ const FriendItem = ({ userId, displayName, email, onRemove }: FriendItemProps) =
                 <span className="truncate">{email}</span>
             </div>
             <button
-                onClick={async () => {
+                onClick={async (e) => {
+                    e.stopPropagation();
+                    e.preventDefault();
                     if (await removeFriendClick())
                         onRemove();
                 }}
                 className="bg-[var(--color-bad)] rounded-lg p-2 md:p-3 hover:bg-[var(--color-bad-accent)] w-max justify-self-end aspect-square md:text-1xl">
                 <span className="inline">&#x2A2F;</span>
             </button>
-        </div>
+        </Link>
     )
 };
     
